e5fc4adb4b4e03d9c5ad6175475c514a56118dd9
shota-hibino <yamoushou@gmail.com>
2021-06-14 06:52:28 +0000

    [Update]login function add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def after_sign_in_path_for(resource)
+  #   case resource
+  #   when Admin
+  #     admin_orders_path
+  #   when Member
+  #     public_member_path(current_member)
+  #   end
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   if resource == :member
+  #     public_root_path
+  #   elsif resource == :admin
+  #     new_admin_session_path
+  #   else
+  #     root_path
+  #   end
+  # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana])
+  end
 end
--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,16 +1,29 @@
 class Public::MembersController < ApplicationController
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to public_member_path(@member.id), notice:"登録情報を変更しました。"
+    else
+      render :edit
+    end
   end
 
   def secession_view
   end
 
   def secession
+  end
+
+   private
+  def member_params
+    params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email)
   end
 end
