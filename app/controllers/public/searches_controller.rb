824cad6f06aca6b07dc56bfcfc1191ddb64eb516
shota-hibino <yamoushou@gmail.com>
2021-06-23 11:39:39 +0000

    [Update]layout fixing

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,7 +1,6 @@
 class Public::SearchesController < ApplicationController
 
 	def search
-		@dog_breeds = DogBreed.all.page(params[:page]).per(8)
 		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
